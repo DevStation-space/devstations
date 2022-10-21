05e700cd51eeb39d6db00890ab05f63df8158158
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-10-21 22:02:56 +0530

    pages a

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
 import Logo from "../assets/images/logo.png"
+import styles from  "../styles/Header.module.css"
 const Header = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
         <div>
             <Image
             src={Logo}
@@ -11,6 +12,25 @@ const Header = () => {
             height={100}
             width={100}
             />
+        </div>
+        <div>
+            <ul>
+                <li>
+                    Community 
+                </li>
+                <li>
+                    CLub
+                </li>
+                <li>
+                    Events
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Login
+                </li>
+            </ul>
         </div>
     </div>
   )
