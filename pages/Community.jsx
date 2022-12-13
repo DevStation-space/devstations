efddbb79fe80f4dd42f5b57b80745dea35b01543
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-12-13 23:59:47 +0530

    init change

--- pages/Community.jsx
+++ pages/Community.jsx
@@ -4,31 +4,14 @@ import { FaWhatsapp, FaDiscord } from 'react-icons/fa';
 import Image from 'next/image'
 import Img1 from '../assets/images/Frame 3.png'
 import Particle from '../components/Particle'
-
+import Link from 'next/link'
 import Whatweoffer from '../components/Whatweoffer';
 import Teams from '../components/Teams';
+import Header from '../components/Header';
+
 const Community = () => {
   return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-      <div className={styles.backImg}>
-        <Image src={Img1} alt="images" />
-      </div>
-      </div>
-      <div className={styles.para2}>
-        <div className={styles.para1}>
-      <p>Join Our Community Related Content</p>
-      </div>
-      <div className={styles.icons}>
-      <div className={styles.icon1}>
-        <FaWhatsapp/>
-        Join Whatsapp
-      </div>
-      <div className={styles.icon2}>
-          <FaDiscord/>
-        Join Discord
-      </div>
-      </div>
+    
     <>
       <Header />
       <div className={styles.background}>
@@ -61,14 +44,9 @@ const Community = () => {
         <Whatweoffer />
         <Teams />
 
-      </div>
-      
-      
-      <Whatweoffer/>
-
-      <Teams/>
       
     </div>
+    </>
      
   )
 }
