2c7f64ba7e58a73e647d64628617a5d5825dcb67
Shalini yadav <shaliniyadav0203@gmail.com>
2022-12-15 21:23:11 +0530

    main page updated

--- components/Events.jsx
+++ components/Events.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from '../styles/Events.module.css'
+import Reach from './Reach'
 
 const Events = () => {
   return (
-    <div>Events</div>
+  <>
+    <div className={styles.heading}>
+      <h1>EVENTS</h1>
+    </div>
+    <div className={styles.box}>
+    <div className={styles.boxes1}></div>
+    <div className={styles.boxes2}></div>
+    <div className={styles.boxes3}></div>
+    </div>
+    <div className={styles.boxes4}>
+      <h1>More to Come!!!</h1>
+    </div>
+    <Reach/>
+    </>
   )
 }
 
--- components/Reach.jsx
+++ components/Reach.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from '../styles/Reach.module.css'
 
 const Reach = () => {
   return (
-    <div>Reach</div>
+    <>
+    <div className={styles.reach}>
+      <h1>OUR REACH</h1>
+    </div>
+    </>
   )
 }
 
