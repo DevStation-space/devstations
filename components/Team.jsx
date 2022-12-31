74f75f56eb708abf0b77c162972d6e91e8fc348d
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-12-31 17:13:12 +0530

    team

--- components/Team.jsx
+++ components/Team.jsx
@@ -25,28 +25,28 @@ const Team = () => {
             <h2 className={styles.subtitle}>{member.role}</h2>
             <div className={styles.social}>
               <ul className={styles.socialItem}>
-                <Link href={member.social.github}>
+                <Link href={member.social.github} alt={member.name} rel='noopener noreferrer' target="_blank">
                   <li className={styles.social__github}>
                     <FaGithub />
                   </li>
                 </Link>
-                <Link href={member.social.twitter}>
+                <Link href={member.social.twitter} alt={member.name} rel='noopener noreferrer' target="_blank" >
                   <li className={styles.social__twitter}>
                     <FaTwitter />
                   </li>
                 </Link>
-                <Link href={member.social.linkedin}>
+                <Link href={member.social.linkedin} alt={member.name} rel='noopener noreferrer' target="_blank">
                   <li className={styles.social__linkedin}>
                     <FaLinkedin />
                   </li>
                 </Link>
-                <Link href={member.social.instagram}>
+                <Link href={member.social.instagram} alt={member.name} rel='noopener noreferrer' target="_blank">
                   <li className={styles.social__instagram}>
                     <FaInstagram />
                   </li>
                 </Link>
 
-                <Link href={`mailto:${member.email}`}>
+                <Link href={`mailto:${member.email}`} alt={member.name} rel='noopener noreferrer' target="_blank">
                   <li className={styles.social__email}>
                     <HiOutlineMail />
                   </li>
@@ -54,7 +54,103 @@ const Team = () => {
               </ul>
             </div>
           </div>
-        ))}
+        ))[0]}
+
+{memberData.map((member) => (
+          <div className={styles.teamMember} key={member.id}>
+            <Image
+              src={`${member.image}`}
+              alt={member.name}
+              height={200}
+              width={200}
+              className={styles.teamMember__img}
+            />
+            <h1 className={styles.title}>{member.name}</h1>
+            <h2 className={styles.subtitle}>{member.role}</h2>
+            <div className={styles.social}>
+              <ul className={styles.socialItem}>
+                <Link href={member.social.github} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__github}>
+                    <FaGithub />
+                  </li>
+                </Link>
+                <Link href={member.social.twitter} alt={member.name} rel='noopener noreferrer' target="_blank" >
+                  <li className={styles.social__twitter}>
+                    <FaTwitter />
+                  </li>
+                </Link>
+                <Link href={member.social.linkedin} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__linkedin}>
+                    <FaLinkedin />
+                  </li>
+                </Link>
+                <Link href={member.social.instagram} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__instagram}>
+                    <FaInstagram />
+                  </li>
+                </Link>
+
+                <Link href={`mailto:${member.email}`} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__email}>
+                    <HiOutlineMail />
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        ))[1]}
+
+{memberData.map((member) => (
+          <div className={styles.teamMember} key={member.id}>
+            <Image
+              src={`${member.image}`}
+              alt={member.name}
+              height={200}
+              width={200}
+              className={styles.teamMember__img}
+            />
+            <h1 className={styles.title}>{member.name}</h1>
+            <h2 className={styles.subtitle}>{member.role}</h2>
+            <div className={styles.social}>
+              <ul className={styles.socialItem}>
+                <Link href={member.social.github} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__github}>
+                    <FaGithub />
+                  </li>
+                </Link>
+                <Link href={member.social.twitter} alt={member.name} rel='noopener noreferrer' target="_blank" >
+                  <li className={styles.social__twitter}>
+                    <FaTwitter />
+                  </li>
+                </Link>
+                <Link href={member.social.linkedin} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__linkedin}>
+                    <FaLinkedin />
+                  </li>
+                </Link>
+                <Link href={member.social.instagram} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__instagram}>
+                    <FaInstagram />
+                  </li>
+                </Link>
+
+                <Link href={`mailto:${member.email}`} alt={member.name} rel='noopener noreferrer' target="_blank">
+                  <li className={styles.social__email}>
+                    <HiOutlineMail />
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        ))[2]}
+      </div>
+      <div className="text-center justify-center flex px-5 py-3 ">
+        <Link href='/Teams'>
+        <button className="bg-blue-600 text-xl px-2 py-1 rounded-lg hover:bg-blue-900 shadow-yellow-300 shadow-md" >
+            Load More
+        </button>
+        </Link>
+        
       </div>
     </div>
   );
---
+++ pages/Teams.jsx
@@ -0,0 +1,102 @@
+import Head from "next/head";
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Team.module.css";
+import dta from "../components/team.json";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import Particle from "../components/Particle";
+const Teams = () => {
+  const memberData = dta.teamMembers.memberData;
+  return (
+    <>
+      <Particle />
+      <Head>
+        <title>Teams</title>
+      </Head>
+      <Header />
+      <div className={styles.team}>
+        <div className={styles.Stats__head}>Our Team</div>
+        <div>
+          <div className={styles.wrapper}>
+            {memberData.map((member) => (
+              <div className={styles.teamMember} key={member.id}>
+                <Image
+                  src={`${member.image}`}
+                  alt={member.name}
+                  height={200}
+                  width={200}
+                  className={styles.teamMember__img}
+                />
+                <h1 className={styles.title}>{member.name}</h1>
+                <h2 className={styles.subtitle}>{member.role}</h2>
+                <div className={styles.social}>
+                  <ul className={styles.socialItem}>
+                    <Link
+                      href={member.social.github}
+                      alt={member.name}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <li className={styles.social__github}>
+                        <FaGithub />
+                      </li>
+                    </Link>
+                    <Link
+                      href={member.social.twitter}
+                      alt={member.name}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <li className={styles.social__twitter}>
+                        <FaTwitter />
+                      </li>
+                    </Link>
+                    <Link
+                      href={member.social.linkedin}
+                      alt={member.name}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <li className={styles.social__linkedin}>
+                        <FaLinkedin />
+                      </li>
+                    </Link>
+                    <Link
+                      href={member.social.instagram}
+                      alt={member.name}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <li className={styles.social__instagram}>
+                        <FaInstagram />
+                      </li>
+                    </Link>
+
+                    <Link
+                      href={`mailto:${member.email}`}
+                      alt={member.name}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <li className={styles.social__email}>
+                        <HiOutlineMail />
+                      </li>
+                    </Link>
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Teams;
