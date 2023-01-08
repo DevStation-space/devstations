8f299ae365ab4027fee03a4d5e528248c460970b
Khushi Agarwal <khushiagarwal9274@gmail.com>
2023-01-09 01:47:33 +0530

    designed

--- components/Whatweoffer.jsx
+++ components/Whatweoffer.jsx
@@ -5,16 +5,21 @@ const Whatweoffer = () => {
   return (
     <div className={styles.what__we__offer}>
       <div className={styles.offer}>
-        {/* <span>What Do</span>
+        <span>What Do</span>
           <span> We</span> 
           <span> Offer ?</span>
         </div>
         <div className={styles.container}>
-        <div className={styles.flex}>1</div>
-        <div className={styles.flex}>2</div>
-        <div className={styles.flex}>3</div>
-        </div> */}
-        <div className={styles.para2}>
+        <div className={styles.flex}>Jobs and internships</div>
+        <div className={styles.flex}>Hackathons</div>
+        <div className={styles.flex}>Meet ups</div>
+        <div className={styles.flex}>Mentorship</div>
+        <div className={styles.flex}>Workshops from industry experts</div>
+        <div className={styles.flex}>Industrial visits</div>
+        <div className={styles.flex}>Open source projects</div>
+       </div> 
+
+       {/* <div className={styles.para2}>
           <h2>What We Offer</h2>
           <p>
             Our community offers a variety of resources and support for
@@ -26,9 +31,9 @@ const Whatweoffer = () => {
             <li>Access to exclusive events and workshops</li>
             <li>Mentorship and career development support</li>
           </ul>
-        </div>
+  </div>*/}
       </div>
-    </div>
+    
   );
 };
 
