daf3ca226da8f2026e2555c29d24a0c0656b2a98
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2023-02-24 11:41:31 +0530

    chnages init

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <meta name="description" content="DevStation is an Open Source Community that helps tech enthusiasts to grow their self in tech. It provides free resources for the developers as well as the beginners. We provide a platform for the developers to discuss their ideas and projects. " />
         <meta name="keywords" content="community, hackathon,events,workshop, session, job, internship " />
         <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
       </Head>
       <Header />
